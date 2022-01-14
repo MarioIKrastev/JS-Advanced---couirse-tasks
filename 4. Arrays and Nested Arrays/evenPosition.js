4a603d00d03c9d3f6b27e29f0a15f2246e79c85c
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-14 20:11:54 +0200

    Add files via upload

---
+++ 4. Arrays and Nested Arrays/evenPosition.js
@@ -0,0 +1,8 @@
+function even(input) {
+  let result = [];
+  for (let i = 0; i < input.length; i += 2) {
+    result.push(input[i])
+  }
+  console.log(result.join(' '));
+}
+even(["20", "30", "40", "50", "60"]);
