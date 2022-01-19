92ce5b82201e0fa6ac3b52d0a5903a9addcab86e
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-19 22:46:37 +0200

    Create 6ListOfNames.js

---
+++ 4. Arrays and Nested Arrays/Exercises/6ListOfNames.js
@@ -0,0 +1,9 @@
+function listOfNames(input) {
+  const sorted = input.sort((a, b) => {
+    return a.localeCompare(b);
+  });
+  for (let i = 0; i < sorted.length; i++) {
+    console.log(`${i + 1}.${sorted[i]}`);
+  }
+}
+listOfNames(["John", "Bob", "Christina", "Ema"]);
