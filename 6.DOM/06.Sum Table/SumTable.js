f0869a35a4f53b5d202986a3e8b34280d0670a14
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-23 19:32:40 +0200

    Update SumTable.js

--- 6.DOM/06.Sum Table/SumTable.js
+++ 6.DOM/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
-
+  let priceElements = document.querySelectorAll(
+    "tr:not(:last-child) td:nth-of-type(2)"
+  );
+  let result = 0;
+  for (const x of priceElements) {
+    let number = +x.textContent;
+    result += number;
+  }
+  document.getElementById("sum").textContent += result;
 }
