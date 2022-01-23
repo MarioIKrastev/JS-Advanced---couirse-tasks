6842ec1db2ee2a3af60f83e68ba8d3ee08ac475d
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-23 17:22:43 +0200

    Update ColorizeTable.js

--- 6.DOM/05.Colorize Table/ColorizeTable.js
+++ 6.DOM/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+  let result = document.querySelectorAll("tr:nth-of-type(2n)");
+  for (const x of result) {
+    x.style.backgroundColor = "teal";
+  }
 }
+/*result.forEach((x)=>{
+x.style.backgroundColor = "teal";
+})
