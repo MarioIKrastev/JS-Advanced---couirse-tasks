974cc9efa7fc057d9c58219c0143056cdcf2e2ff
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-24 10:08:08 +0200

    Update ExtractParenthesis.js

--- 6.DOM/07.Extract Parenthesis/ExtractParenthesis.js
+++ 6.DOM/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,10 @@
 function extract(content) {
-
+  let extractedElements = document.getElementById(content);
+  let regEx = /\(([^(]+)\)/g;
+  let matches = extractedElements.textContent.matchAll(regEx);
+  let result = [];
+  for (const x of matches) {
+    result.push(x[1]);
+  }
+  return result.join("; ");
 }
