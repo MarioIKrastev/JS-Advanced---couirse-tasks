39d4eaae633c3c3ea7e8c5791427fd7c277a5357
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-27 00:02:35 +0200

    Update solution.js

--- 6.DOM/Excersices/02. Pascal or Camel Case/solution.js
+++ 6.DOM/Excersices/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+  let namingConventionElement = document.getElementById("naming-convention")
+    .value;
+  let textElement = document.getElementById("text").value;
+
+  let resultElement = document.getElementById("result");
+
+  if (namingConventionElement == "Camel Case") {
+    textElement = textElement.toLowerCase().split(" ");
+
+    let result = "";
+    for (let i = 0; i < textElement.length; i++) {
+      i == 0
+        ? (result = textElement[i])
+        : (result +=
+            textElement[i].charAt(0).toUpperCase() + textElement[i].slice(1));
+    }
+
+    resultElement.textContent = result;
+  } else if (namingConventionElement == "Pascal Case") {
+    textElement = textElement.toLowerCase().split(" ");
+    let result = "";
+    for (const x of textElement) {
+      result += x.charAt(0).toUpperCase() + x.slice(1);
+    }
+    resultElement.textContent = result;
+  } else {
+    resultElement.textContent = "Error!";
+  }
 }
