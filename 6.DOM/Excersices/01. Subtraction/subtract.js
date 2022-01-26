e4019e395a47193876c57843c59f59f80d261110
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-26 16:36:20 +0200

    Update subtract.js

--- 6.DOM/Excersices/01. Subtraction/subtract.js
+++ 6.DOM/Excersices/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+  let firstNumberElement = document.getElementById("firstNumber").value;
+  let secondNumberElement = document.getElementById("secondNumber").value;
+  let resultElement = document.getElementById("result");
+  resultElement.textContent = +firstNumberElement - +secondNumberElement;
 }
