06c8e8b41cbdb112a75d856508991fe26a4d490c
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-29 18:06:51 +0200

    Update solution.js

--- 6.DOM/Excersices/05. Table - Search Engine/solution.js
+++ 6.DOM/Excersices/05. Table - Search Engine/solution.js
@@ -1,8 +1,33 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  let containerItemsElement = document.querySelectorAll(
+    ".container tbody tr td"
+  );
+  let inputElement = document.getElementById("searchField");
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    for (const x of containerItemsElement) {
+      if (
+        x.textContent.includes(inputElement.value) &&
+        inputElement.value != ""
+      ) {
+        const parentItemElement = x.closest("tr");
+        const classListElement = parentItemElement.classList;
+        classListElement.add("select");
+      }
+    }
+    inputElement.value = "";
+    document
+      .getElementById("searchField")
+      .addEventListener("click", onClickInput);
+    function onClickInput() {
+      let tableRowElement = document.querySelectorAll(".container tbody tr");
 
-   }
+      for (const m of tableRowElement) {
+        if (m.classList.contains("select")) {
+          m.classList.remove("select");
+        }
+      }
+    }
+  }
 }
