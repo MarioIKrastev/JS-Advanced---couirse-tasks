49135faf2029f162d1cf380f4af6ea6355a1b816
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-29 14:30:43 +0200

    Update search.js

--- 6.DOM/Excersices/04. Search in List/search.js
+++ 6.DOM/Excersices/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+  let listElements = document.getElementById("towns");
+  let searchInputElement = document.getElementById("searchText");
+  let resultElement = document.getElementById("result");
+
+  let arrayWithTowns = Array.from(listElements.children);
+
+  let matchCounter = 0;
+  for (const item of arrayWithTowns) {
+    if (item.textContent.includes(searchInputElement.value)) {
+      item.style.fontWeight = "bold";
+      item.style.textDecoration = "underline";
+      matchCounter += 1;
+    }
+  }
+  searchInputElement.value = "";
+  resultElement.textContent = `${matchCounter} matches found`;
 }
