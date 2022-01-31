a5110664b48fb83d08488135b274bd0eb820f76a
MarioIKrastev <85845536+MarioIKrastev@users.noreply.github.com>
2022-01-31 13:39:31 +0200

    Update solution.js

--- 6.DOM/Excersices/06. Format the Text/solution.js
+++ 6.DOM/Excersices/06. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  let inputElement = document.getElementById('input').value.split('.').filter(e => e!=='');
+  let outputElement = document.getElementById('output');
+
+
+while(inputElement.length > 0){
+  let pElement = document.createElement('p');
+  let textElements = inputElement.splice(0, 3).join('. ') + '.';
+  pElement.textContent = textElements;
+  outputElement.appendChild(pElement);
+  }
+
 }
